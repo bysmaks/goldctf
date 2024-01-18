c165343c6be2a70e98644ba41a8e911f33258e83
Boris Karabut <boriskarabytwork@gmail.com>
2024-01-18 11:44:58 +0300

    Попытка фикса прода N4

---
+++ dist/_next/static/9m_LxDRO3vXdIOpacrj1s/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:void 0,source:e,destination:e}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e87e5963ec1b8011.js"],sortedPages:["/_app","/_error"]}}("/goals"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
